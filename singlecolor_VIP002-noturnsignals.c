935d12e244c552985424347b8f63fe80c3484878
outerwear <mcoates@wayfair.com>
2017-08-06 15:22:50 -0400

    Initial commit, adding old code from my Subaru!

---
+++ singlecolor_VIP002-noturnsignals.c
@@ -0,0 +1,283 @@
+/*
+ * LED Control code for brake lights, turn signals, and VIP
+ * Author: Michael Coates
+ * v001   20150714    File created.
+ */
+
+#include <SPI.h>
+ 
+#define NUMLED 16
+#define LASTLED 15
+
+// Global variables
+const int VIP = 5;
+const int latchPin = 10;
+const int clockPin = 13;
+const int dataPin = 11;
+const int tlp = 9;
+const int tcp = 12;
+const int tdp = 8;
+int outputEnablePin = 3;
+int VIPmode    = 0;
+boolean registers[NUMLED]; //red
+int SS1 = 10;
+
+void setup(){
+  pinMode(latchPin, OUTPUT);
+  pinMode(outputEnablePin, OUTPUT);
+  pinMode(tlp, OUTPUT);
+  //pinMode(tcp, OUTPUT);
+  pinMode(tdp, OUTPUT);
+  pinMode(VIP, INPUT);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV2);
+}
+
+void loop(){
+  VIPmode = digitalRead(VIP);
+  
+  //if(VIPmode == HIGH) {
+    
+    swipe();
+    stripe_f();
+    swipeL();
+    knight();
+    swipeR();
+    fatten();
+    big_slide();
+    //fade();
+  //}
+  randomSeed(analogRead(A0));
+}
+
+void fade(){
+  for(int x = 0; x < 3; x++){
+    for(int j = 0; j <255; j++){
+      analogWrite(outputEnablePin,j);
+      delay(2);
+    }
+    for(int j = 255; j >= 0; j--){
+      analogWrite(outputEnablePin,j);
+      delay(2);
+    }
+  }
+}
+
+void strobe(){
+  int rled = random(NUMLED);
+  char usedled[NUMLED];
+  memset(usedled,'\0',NUMLED);
+  for(int i = 0; i < NUMLED; i++){
+    while(usedled[rled] == 'x'){
+      rled = random(NUMLED);
+    }
+    usedled[rled] = 'x';
+    for(int j = 0; j < 6; j++){
+      registers[rled] = LOW;
+      //writereg(clockPin, dataPin, latchPin, registers);
+      delay(50);
+      registers[rled] = HIGH;
+      //writereg(clockPin, dataPin, latchPin, registers);
+      delay(50);
+    }
+    rled = random(NUMLED); 
+  }
+}
+
+void swipe(){
+  unsigned int x = 1;
+  unsigned int y = 0;
+  for(int i = 0; i < ((NUMLED/2)*3)+1; i++){
+    SPI.transfer(y);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    SPI.transfer(x);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    if (x < 255 && x != 0){
+      x = (x << 1) | 1;
+    } else if (x > 255 && x < 65280){
+      x = x << 1;
+      y = (y << 1) | 1;
+    } else if (x == 255){
+      x = x << 1;
+      y = 1;
+    } else {
+      y = y << 1;
+    }
+    delay(20);
+  }
+  y = 128;
+  for(int i = 0; i < ((NUMLED/2)*3)+1; i++){
+    SPI.transfer(y);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    SPI.transfer(x);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    if (y > 127 && y < 255){
+      y = (y >> 1) | 128;
+    } else if (y > 0){
+      y = y >> 1;
+      x = (x >> 1) | 128;
+    } else if (y == 255){
+      y = y >> 1;
+      x = 128;
+    } else {
+      x = x >> 1;
+    }
+    delay(20);
+  }
+}
+
+void stripe_f(){
+  unsigned int a = 204;
+  for(int i=0; i<(NUMLED/2); i++){
+    if((i%4) == 0){
+      a = 204; 
+    }
+    for(int j=0; j<5; j++){
+      SPI.transfer(a);
+      digitalWrite(latchPin, LOW);
+      digitalWrite(latchPin, HIGH);
+      SPI.transfer(a);
+      digitalWrite(latchPin, LOW);
+      digitalWrite(latchPin, HIGH);
+      delay(20);
+      SPI.transfer(255);
+      digitalWrite(latchPin, LOW);
+      digitalWrite(latchPin, HIGH);
+      SPI.transfer(255);
+      digitalWrite(latchPin, LOW);
+      digitalWrite(latchPin, HIGH);
+      delay(8);
+      SPI.transfer(a);
+      digitalWrite(latchPin, LOW);
+      digitalWrite(latchPin, HIGH);
+      SPI.transfer(a);
+      digitalWrite(latchPin, LOW);
+      digitalWrite(latchPin, HIGH);
+      delay(20);
+    }
+    a = a >> 1;
+  }
+}
+
+void knight(){
+  unsigned int x = 1, y = 128;
+  for(int i=0; i<NUMLED; i++){
+    SPI.transfer(y);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    SPI.transfer(x);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    delay(55);
+    if(i<(NUMLED/2 - 1)){
+      x = x << 1;
+      y = y >> 1;
+    } else {
+      x = x >> 1;
+      y = y << 1;
+    }
+  }
+}
+
+void fatten(){
+  unsigned int x = 128, y = 1;
+  for(int i=0; i<NUMLED; i++){
+    SPI.transfer(y);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    SPI.transfer(x);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    if(i<(NUMLED/2 - 1)){
+      x = (x >> 1) | 128;
+      y = (y << 1) | 1;
+    } else {
+      x = x << 1;
+      y = y >> 1;
+    }
+    delay(30);
+  }
+}
+
+void big_slide(){
+// 00001111
+// 11110000
+  unsigned int a = 0, b = 255;
+  for(int i=0; i<8; i++){
+    SPI.transfer(b);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    SPI.transfer(a);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    a = (a << 1) | 1;
+    b = (b << 1);
+    delay(50);
+  }
+  a = 255;
+  b = 0;
+  for(int i=0; i<8; i++){
+    SPI.transfer(b);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    SPI.transfer(a);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    a = (a >> 1);
+    b = (b >> 1) | 128;
+    delay(50);
+  }
+}
+
+void swipeL(){
+  unsigned int x = 1;
+  unsigned int y = 0;
+  for(int i = 0; i < ((NUMLED/2)*3)+1; i++){
+    SPI.transfer(y);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    SPI.transfer(x);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    if (x < 255 && x != 0){
+      x = (x << 1) | 1;
+    } else if (x > 255 && x < 65280){
+      x = x << 1;
+      y = (y << 1) | 1;
+    } else if (x == 255){
+      x = x << 1;
+      y = 1;
+    } else {
+      y = y << 1;
+    }
+    delay(20);
+  }
+}
+void swipeR(){
+  unsigned int x = 0, y = 128;
+  for(int i = 0; i < ((NUMLED/2)*3)+1; i++){
+    SPI.transfer(y);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    SPI.transfer(x);
+    digitalWrite(latchPin, LOW);
+    digitalWrite(latchPin, HIGH);
+    if (y > 127 && y < 255){
+      y = (y >> 1) | 128;
+    } else if (y > 0){
+      y = y >> 1;
+      x = (x >> 1) | 128;
+    } else if (y == 255){
+      y = y >> 1;
+      x = 128;
+    } else {
+      x = x >> 1;
+    }
+    delay(20);
+  }
+}
+
---
+++ ws2812b_brakelightv2.c
@@ -0,0 +1,320 @@
+/* 
+ *  Author: Michael Coates 
+ *  Purpose: WS2812B LED tail lights for SUBARU IMPREZA WRX/STI GR (HATCH)
+ *  
+ */
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 5
+#define NUMPIXELS 56
+
+const int buttonPin = 2;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripb = Adafruit_NeoPixel(NUMPIXELS, PIN+1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripc = Adafruit_NeoPixel(NUMPIXELS+21, PIN+5, NEO_GRB + NEO_KHZ800);
+float vPow = 4.7;
+float r1 = 100000;
+float r2 = 10000;
+int buttonState = 0;
+ 
+void setup() {
+  pinMode(buttonPin, INPUT);
+   /*Serial.begin(9600);
+   Serial.print("\x1B");
+   Serial.print("[2J");
+   Serial.print("\x1B");
+   Serial.println("[H");
+   Serial.println("--------------------");
+   Serial.println("DC VOLTMETER");
+   Serial.print("Maximum Voltage: ");
+   Serial.print((int)(vPow / (r2 / (r1 + r2))));
+   Serial.println("V");
+   Serial.println("--------------------");
+   Serial.println("");
+*/
+   delay(4000);
+   strip.begin();
+   stripb.begin();
+   stripc.begin();
+   strip.show();
+   stripb.show();
+   stripc.show();  
+   startup_seq();  
+ }
+ 
+void loop() {
+   float v = (analogRead(0) * vPow) / 1024.0; // parking lights (green wire)
+   float v2 = v / (r2 / (r1 + r2));
+   float z = (analogRead(1) * vPow) / 1024.0; // brakes (red wire)
+   float z2 = z / (r2 / (r1 + r2));
+   
+/*   Serial.print("\x1B");
+   Serial.print("[1A");  
+   Serial.println(v2);
+*/ 
+  if(z2 > 2.5){
+    for(int i=0;i<NUMPIXELS+21;i++){
+      if(i<NUMPIXELS){
+      strip.setPixelColor(i, strip.Color(245,0,0)); 
+      stripb.setPixelColor(i, stripb.Color(245,0,0));
+      stripc.setPixelColor(i, stripc.Color(245,0,0));
+      } else {
+        stripc.setPixelColor(i, stripc.Color(245,0,0));
+      }
+    }
+    strip.show();
+    stripb.show();
+    stripc.show();
+  } else {
+    if(v2 > 2.5){
+      for(int i=0;i<NUMPIXELS+21;i++){
+        if(i<NUMPIXELS){
+          strip.setPixelColor(i, strip.Color(70,0,0)); 
+          stripb.setPixelColor(i, stripb.Color(70,0,0));
+          stripc.setPixelColor(i, stripc.Color(70,0,0));
+        } else {
+          stripc.setPixelColor(i, stripc.Color(70,0,0));
+        }
+      }
+      strip.show();
+      stripb.show();
+      stripc.show();
+    } else {
+      for(int i=0;i<NUMPIXELS+21;i++){
+        if(i<NUMPIXELS){
+          strip.setPixelColor(i, strip.Color(0,0,0));
+          stripb.setPixelColor(i, stripb.Color(0,0,0));
+          stripc.setPixelColor(i, stripc.Color(0,0,0));
+        } else {
+          stripc.setPixelColor(i, stripc.Color(0,0,0));
+        }
+      }
+      strip.show();
+      stripb.show();
+      stripc.show();
+    }
+  }  
+
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == HIGH){
+   /* colorWipe(strip.Color(255, 0, 0), 50); // Red
+    colorWipe(strip.Color(0, 255, 0), 50); // Green
+    colorWipe(strip.Color(0, 0, 255), 50); // Blue */
+    theaterChase(strip.Color(127, 127, 127), 50); // White
+    theaterChase(strip.Color(127, 0, 0), 50); // Red
+    theaterChase(strip.Color(0, 0, 127), 50); // Blue 
+    rainbow(20);
+    rainbowCycle(20);
+    theaterChaseRainbow(50);
+    colorfade(10);
+    buttonState = LOW;
+  } 
+}
+
+void startup_seq(){
+  int i;
+  for(i=0; i < NUMPIXELS; i++){
+    strip.setPixelColor(i, strip.Color(250,0,0));
+    stripb.setPixelColor(i, stripb.Color(250,0,0));
+    stripc.setPixelColor(i, stripc.Color(250,0,0));
+    strip.show();
+    stripb.show();
+    stripc.show();
+    delay(15);
+    strip.setPixelColor(i, strip.Color(0,250,0));
+    stripb.setPixelColor(i, stripb.Color(0,250,0));
+    stripc.setPixelColor(i, stripc.Color(0,250,0));
+    strip.show();
+    stripb.show();
+    stripc.show();
+    delay(15);
+    strip.setPixelColor(i, strip.Color(0,0,250));
+    stripb.setPixelColor(i, stripb.Color(0,0,250));
+    stripc.setPixelColor(i, stripc.Color(0,0,250));
+    strip.show();
+    stripb.show();
+    stripc.show();
+    delay(15);
+    strip.setPixelColor(i, strip.Color(0,0,0));
+    stripb.setPixelColor(i, stripb.Color(0,0,0));
+    stripc.setPixelColor(i, stripc.Color(0,0,0));
+    strip.show();
+    stripb.show();
+    stripc.show();
+    delay(15);
+  }
+  for(i=NUMPIXELS; i<NUMPIXELS+21; i++){
+    stripc.setPixelColor(i, stripc.Color(250,0,0));
+    stripc.show();
+    delay(15);
+    stripc.setPixelColor(i, stripc.Color(0,250,0));
+    stripc.show();
+    delay(15);
+    stripc.setPixelColor(i, stripc.Color(0,0,250));
+    stripc.show();
+    delay(15);
+    stripc.setPixelColor(i, stripc.Color(0,0,0));
+    stripc.show();
+    delay(15);
+  }
+}
+
+void flash(){
+  int i, j, k;
+
+
+}
+
+void colorfade(uint8_t wait){
+  int k;
+  for(k=0; k < 250; k++){
+    for(uint16_t i=0; i<NUMPIXELS; i++){
+      strip.setPixelColor(i,strip.Color(k,k,0));
+      stripb.setPixelColor(i,stripb.Color(k, 0, k));
+      strip.show();
+      stripb.show();
+    }
+    delay(wait);
+  }
+  for(k=0; k < 250; k++){
+    for(uint16_t i=0; i<NUMPIXELS; i++){
+      strip.setPixelColor(i,strip.Color(0,k,k));
+      stripb.setPixelColor(i,stripb.Color(k, k/3, k/2));
+      strip.show();
+      stripb.show();
+    }
+    delay(wait);
+  }
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    stripb.setPixelColor(i, c);
+    strip.show();
+    stripb.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+      stripb.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    stripb.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      stripb.setPixelColor(i, Wheelb(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    stripb.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+        stripb.setPixelColor(i+q, c);
+      }
+      strip.show();
+      stripb.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        stripb.setPixelColor(i+q, 0);
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+        stripb.setPixelColor(i+q, Wheelb( (i+j) % 255));
+      }
+      strip.show();
+      stripb.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        stripb.setPixelColor(i+q, 0);
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+uint32_t Wheelb(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return stripb.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return stripb.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return stripb.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void flash(uint32_t col, uint16_t num, int pix, uint8_t flag){
+  for(uint16_t i=0; i < num; i++){
+    if(flag){
+      strip.setPixelColor(pix,strip.Color(0,0,0));
+      strip.show();
+      delay(8);
+      strip.setPixelColor(pix,col);
+      strip.show();
+    } else {
+      stripb.setPixelColor(pix,strip.Color(0,0,0));
+      stripb.show();
+      delay(8);
+      stripb.setPixelColor(pix,col);
+      stripb.show();
+    }
+  }
+}
+
